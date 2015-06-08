1202e19d8864015266d31e90a537d13da51bf9a9
Bruce Marriner <bruce@bmts.us>
2015-06-08 09:18:43 -0500

    Moved some settings into a config file, cleaned up code a little bit.

--- rxfax.lua
+++ rxfax.lua
@@ -1,3 +1,8 @@
+-- Load local configuration file
+-- somehow it should create this file
+-- with defaults on install..
+loadfile("cfgfax")
+
 
 -- fetch a couple of channel variables needed to get started.
 uuid                 = session:getVariable("uuid")
@@ -6,7 +11,7 @@ destination_number   = session:getVariable("destination_number")
 
 
 -- console notification that we're taking a call.
-freeswitch.consoleLog("info","fsfax: " .. uuid .. ": Taking call from " .. caller_id_number .. " for " .. destination_number .. "\n")
+freeswitch.consoleLog("info",uuid .. " fsfax: " .. uuid .. ": Taking call from " .. caller_id_number .. " for " .. destination_number .. "\n")
 
 
 local dbh = freeswitch.Dbh("sqlite://fsfax")  -- /var/lib/freeswitch/db/fsfax.db
@@ -21,12 +26,15 @@ query = "SELECT key, value FROM fsfax_config "
 ..      "   OR (match_key='DID' AND match_value='" .. destination_number .. "') " 
 ..      "   OR (match_key='CID' AND match_value='" .. caller_id_number .. "') "
 ..      "    ) "
-..      "order by match_key, match_value, type, key, rank"
+..      "ORDER BY match_key, match_value, type, key, rank"
 
+
+
+freeswitch.consoleLog("info",uuid .. " fsfax: call settings query \n" .. query .. "\n")
 
 dbh:query(query, 
 	function(row)
-	freeswitch.consoleLog("info","fsfax: setting session key " .. row.key .. " => " .. row.value .. "\n")
+	freeswitch.consoleLog("info",uuid .. " fsfax: setting session " .. row.key .. " => " .. row.value .. "\n")
 		session:setVariable(row.key, row.value)
 	end)
 
@@ -84,89 +92,72 @@ and send out an e-mail notification then log the fax call.
 
 
 -- http://wiki.freeswitch.org/wiki/Channel_Variables
-caller_id_name       = session:getVariable("caller_id_name")
-context              = session:getVariable("context");
-network_addr         = session:getVariable("network_addr");
-ani                  = session:getVariable("ani");
-aniii                = session:getVariable("aniii");
-rdnis                = session:getVariable("rdnis");
-source               = session:getVariable("source");
-chan_name            = session:getVariable("chan_name");
-created_time         = session:getVariable("created_time");
-
-ignore_early_media   = session:getVariable("ignore_early_media"); 
-if (ignore_early_media == nil) then ignore_early_media = ''; end
+caller_id_name      = session:getVariable("caller_id_name")
+context             = session:getVariable("context");
+network_addr        = session:getVariable("network_addr");
+ani                 = session:getVariable("ani");
+aniii               = session:getVariable("aniii");
+rdnis               = session:getVariable("rdnis");
+source              = session:getVariable("source");
+chan_name           = session:getVariable("chan_name");
+created_time        = session:getVariable("created_time");
+ignore_early_media  = session:getVariable("ignore_early_media"); 
+t38_leg             = session:getVariable("t38_leg");
+fax_bad_rows        = session:getVariable("fax_bad_rows")
 
 -- wiki.freeswitch.org/wiki/Mod_spandsp
-t38_leg = session:getVariable("t38_leg");
-if (t38_leg == nil) then t38_leg = ''; end
-
-fax_bad_rows = session:getVariable("fax_bad_rows")
-if (fax_bad_rows == nil) then fax_bad_rows = ''; end
-
-fax_document_total_pages = session:getVariable("fax_document_total_pages")
-if (fax_document_total_pages == nil) then fax_document_total_pages = ''; end
-
+fax_document_total_pages       = session:getVariable("fax_document_total_pages")
 fax_document_transferred_pages = session:getVariable("fax_document_transferred_pages")
+fax_ecm_requested              = session:getVariable("fax_ecm_requested") 
+fax_ecm_used                   = session:getVariable("fax_ecm_used")
+fax_filename                   = session:getVariable("fax_filename")
+fax_image_resolution           = session:getVariable("fax_image_resolution")
+fax_image_size                 = session:getVariable("fax_image_size")
+fax_local_station_id           = session:getVariable("fax_local_station_id")
+fax_result_code                = session:getVariable("fax_result_code")
+fax_result_text                = session:getVariable("fax_result_text")
+fax_remote_station_id          = session:getVariable("fax_remote_station_id")
+fax_success                    = session:getVariable("fax_success")
+fax_transfer_rate              = session:getVariable("fax_transfer_rate")
+fax_v17_disabled               = session:getVariable("fax_v17_disabled")
+jitterbuffer_msec              = session:getVariable("jitterbuffer_msec") 
+rtp_autoflush_during_bridge    = session:getVariable("rtp_autoflush_during_bridge") 
+t38_gateway_format             = session:getVariable("t38_gateway_format") 
+t38_peer                       = session:getVariable("t38_peer");  
+t38_trace_read                 = session:getVariable("t38_trace_read"); 
+
+-- There must be a better way, but this prevents errors below
+-- when using variables incase they are null.
+if (ignore_early_media == nil)             then ignore_early_media = ''; end
+if (t38_leg == nil)                        then t38_leg = ''; end
+if (fax_bad_rows == nil)                   then fax_bad_rows = ''; end
+if (fax_document_total_pages == nil)       then fax_document_total_pages = ''; end
 if (fax_document_transferred_pages == nil) then fax_document_transferred_pages = ''; end
-
-fax_ecm_requested = session:getVariable("fax_ecm_requested") 
-if (fax_ecm_requested == nil) then fax_ecm_requested = ''; end
-
-fax_ecm_used = session:getVariable("fax_ecm_used")
-if (fax_ecm_used == nil) then fax_ecm_used = ''; end
-
-fax_filename = session:getVariable("fax_filename")
-if (fax_filename == nil) then fax_filename = ''; end
-
-fax_image_resolution = session:getVariable("fax_image_resolution")
-if (fax_image_resolution == nil) then fax_image_resolution = ''; end
-
-fax_image_size = session:getVariable("fax_image_size")
-if (fax_image_size == nil) then fax_image_size = ''; end
-
-fax_local_station_id = session:getVariable("fax_local_station_id")
-if (fax_local_station_id == nil) then fax_local_station_id = ''; end
-
-fax_result_code = session:getVariable("fax_result_code")
-if (fax_result_code == nil) then fax_result_code = ''; end
-
-fax_result_text = session:getVariable("fax_result_text")
-if (fax_result_text == nil) then fax_result_text = ''; end
-
-fax_remote_station_id = session:getVariable("fax_remote_station_id")
-if (fax_remote_station_id == nil) then fax_remote_station_id = ''; end
-
-fax_success = session:getVariable("fax_success")
-if (fax_success == nil) then fax_success = ''; end
-
-fax_transfer_rate = session:getVariable("fax_transfer_rate")
-if (fax_transfer_rate == nil) then fax_transfer_rate  = ''; end
-
-fax_v17_disabled = session:getVariable("fax_v17_disabled")
-if (fax_v17_disabled == nil) then fax_v17_disabled = ''; end
-
-jitterbuffer_msec = session:getVariable("jitterbuffer_msec") 
-if (jitterbuffer_msec == nil) then jitterbuffer_msec = ''; end
-
-rtp_autoflush_during_bridge = session:getVariable("rtp_autoflush_during_bridge") 
-if (rtp_autoflush_during_bridge == nil) then rtp_autoflush_during_bridge = ''; end
-
-t38_gateway_format = session:getVariable("t38_gateway_format") 
-if (t38_gateway_format == nil) then t38_gateway_format = ''; end
-
-t38_peer = session:getVariable("t38_peer");  
-if (t38_peer == nil) then t38_peer = ''; end
-
-t38_trace_read = session:getVariable("t38_trace_read"); 
-if (t38_trace_read == nil) then t38_trace_read = ''; end
+if (fax_ecm_requested == nil)              then fax_ecm_requested = ''; end
+if (fax_ecm_used == nil)                   then fax_ecm_used = ''; end
+if (fax_filename == nil)                   then fax_filename = ''; end
+if (fax_image_resolution == nil)           then fax_image_resolution = ''; end
+if (fax_image_size == nil)                 then fax_image_size = ''; end
+if (fax_local_station_id == nil)           then fax_local_station_id = ''; end
+if (fax_result_code == nil)                then fax_result_code = ''; end
+if (fax_result_text == nil)                then fax_result_text = ''; end
+if (fax_remote_station_id == nil)          then fax_remote_station_id = ''; end
+if (fax_success == nil)                    then fax_success = ''; end
+if (fax_transfer_rate == nil)              then fax_transfer_rate  = ''; end
+if (fax_v17_disabled == nil)               then fax_v17_disabled = ''; end
+if (jitterbuffer_msec == nil)              then jitterbuffer_msec = ''; end
+if (rtp_autoflush_during_bridge == nil)    then rtp_autoflush_during_bridge = ''; end
+if (t38_gateway_format == nil)             then t38_gateway_format = ''; end
+if (t38_peer == nil)                       then t38_peer = ''; end
+if (t38_trace_read == nil)                 then t38_trace_read = ''; end
 
 
 
-to      = emailto
-from    = caller_id_name .. "<" .. caller_id_number .. "@bmts.us>"
+to       = emailto
+from     = caller_id_name .. "<" .. caller_id_number .. "@".. domain ..">"
 body     = "\n\n\n\n---\n"
 ..         "DETAILED FAX INFORMATION:\n"
+..         "Called DID         :" .. destination_number .. "\n"
 ..         "Remote Station ID  :" .. fax_remote_station_id .. "\n"
 ..         "CallerID Number    :" .. caller_id_number .. "\n"
 ..         "CallerID Name      :" .. caller_id_name .. "\n"
@@ -187,7 +178,7 @@ end
 
 if (file_exists(filetiff)) then
 	cmd = "tiff2pdf -z -o " .. filepdf .. " " .. filetiff;
-	freeswitch.consoleLog("info","fsfax: tiff2pdf command: " .. cmd);
+	freeswitch.consoleLog("info",uuid .. " fsfax: tiff2pdf command: " .. cmd .. "\n");
 	os.execute(cmd);
 	attach = filepdf;
 else
@@ -210,7 +201,7 @@ if (attach) then
 	          "Subject: "  .. subject .. "\n" 
 
 	body     = "Your fax is attached to this e-mail.\n" .. body
-	freeswitch.consoleLog("info","fsfax: SENDING EMAIL WITH ATTACHMENT")
+	freeswitch.consoleLog("info", uuid .. " fsfax: SENDING EMAIL WITH ATTACHMENT\n")
 	email = freeswitch.email(to, from, headers, body, attach)
 else
 	if (session:getVariable("faxdetected") == nil) then
@@ -224,15 +215,15 @@ else
 	subject = "[FAX] Receive Failure, " .. fax_result_text .. ", " .. fax_document_total_pages .." Page(s), from " .. caller_id_name .. " " .. fax_remote_station_id;
 
 
+--        	  "To: " .. to .. "\n"  ..
 	headers = "From: "     .. from .. "\n" ..
-        	  "To: " .. to .. "\n"  ..
 	          "Reply-To: " .. from .. "\n"  ..
         	  "Subject: "  .. subject .. "\n" 
 
 
 	body     = "We're sorry but we were unable to receive any fax data from the caller..\n" .. body
-	freeswitch.consoleLog("info","fsfax: SENDING EMAIL -WITHOUT- ATTACHMENT")
-	email = freeswitch.email(to, from, headers, body)
+	freeswitch.consoleLog("info",uuid .. " fsfax: SENDING EMAIL -WITHOUT- ATTACHMENT\n")
+	email = freeswitch.email(admin, from, headers, body)
 end
 
 
@@ -242,6 +233,8 @@ else
 email = "0"
 end
 
+-- TODO: Find a way to make this easier to read and possibly more secure?
+-- Prepared Statement?
 logQuery = "INSERT INTO fsfax_log (uuid"
 ..	   ", type, user, email, emailto, destination_number"
 ..         ", caller_id_number, caller_id_name, context, network_addr, ani"
@@ -264,11 +257,11 @@ logQuery = "INSERT INTO fsfax_log (uuid"
 ..         ", '" .. rtp_autoflush_during_bridge .. "', '" .. t38_gateway_format .. "', '" .. t38_peer .. "', '" .. t38_trace_read .. "'"
 ..         ");"
 
-freeswitch.consoleLog("info","fsfax: logQuery: " .. logQuery);
+freeswitch.consoleLog("info",uuid .. " fsfax: logQuery: \n" .. logQuery .. "\n");
 
 dbh:query(logQuery);
 
 dbh:release();
 
 os.execute("grep " .. uuid .. " /var/log/freeswitch/freeswitch.log > /tmp/FAX-" .. uuid .. ".log")
-freeswitch.consoleLog("info","fsfax: " .. email .. ": Finished Taking call from " .. caller_id_number .. " for " .. destination_number .. "\n")
+freeswitch.consoleLog("info",uuid .. "fsfax: " .. email .. ": Finished Taking call from " .. caller_id_number .. " for " .. destination_number .. "\n")
